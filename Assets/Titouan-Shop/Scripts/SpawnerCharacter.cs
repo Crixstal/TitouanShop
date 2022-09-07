40594400c0bc6a9ac6a27d8076558c5ade21fc69
Manon Mehalin <m.mehalin@student.isartdigital.com>
2022-09-07 10:14:50 +0200

    Begin customer spawn

--- Assets/Titouan-Shop/Scripts/SpawnerCharacter.cs
+++ Assets/Titouan-Shop/Scripts/SpawnerCharacter.cs
@@ -1,34 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Com.IsartDigital.TitouanShop.TitouanShop {
     public class SpawnerCharacter : MonoBehaviour
     {
         [SerializeField] private List<GameObject> allObjects = new List<GameObject>();
-        [SerializeField] private GameObject character;
+        [SerializeField] private GameObject customer;
+        [SerializeField] private Button myButton;
 
+        private Transform spawnRight;
+        private Transform spawnLeft;
+        private Transform spawnMiddle;
+        private Transform spawnFull;
+        
         private List<GameObject> selectObject = new List<GameObject>();
-        private float counter;
+        private List<Transform> spawnList = new List<Transform>();
+
+        private float counter = 0;
 
         private void Start()
         {
+            spawnRight = transform.Find("RightSpawn");
+            spawnLeft = transform.Find("LeftSpawn");
+            spawnMiddle = transform.Find("MiddleSpawn");
+            spawnFull = transform.Find("FullSpawn");
+            
+            //spawnList = Add<>
+
+            ++counter;
+
+            Tuto();
         }
 
         private void Update()
         {
-            //if (counter > 3)
-            //{
-            //    int random = Random.Range(0, color.Count - 1);
-
-            //    allColorAvailable.Add(color[random]);
-            //    color.RemoveAt(random);
-            //}
+            myButton.onClick.AddListener(AddCharacter);
         }
 
-        private void addCharacter(GameObject gameObject)
+        private void AddCharacter()
         {
+            if (canInstantiate())
+                Instantiate(customer, spawnFull);
+        }
 
+        private bool canInstantiate()
+        {
+            if (transform.childCount > 0)
+                return false;
+            
+            return true;
+        }
+        
+        private void Tuto()
+        {
+            // right - left / banane
+            // middle / banane / color
+            // full / banane / all colors
+            // middle / new obj
+            // full
+            // nawak
         }
     }
 }
